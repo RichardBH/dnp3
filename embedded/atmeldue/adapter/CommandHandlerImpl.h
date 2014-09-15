f8068b17b3f8a2a3799f91dc14d0d9ebd581fce7
RichardBH <richard@behan-howell.com>
2014-09-16 08:50:37 +1000

    reinstated override final to CROB functions.

--- embedded/atmeldue/adapter/CommandHandlerImpl.h
+++ embedded/atmeldue/adapter/CommandHandlerImpl.h
@@ -35,24 +35,24 @@ class CommandHandlerImpl : public opendnp3::ICommandHandler, openpal::Uncopyable
 {
 public:
 
-	opendnp3::CommandStatus Supports(const opendnp3::ControlRelayOutputBlock& command, uint16_t index);// override final;
-	opendnp3::CommandStatus Perform(const opendnp3::ControlRelayOutputBlock& command, uint16_t index);// override final;
+	opendnp3::CommandStatus Supports(const opendnp3::ControlRelayOutputBlock& command, uint16_t index) override final;
+	opendnp3::CommandStatus Perform(const opendnp3::ControlRelayOutputBlock& command, uint16_t index) override final;
 
 
-	opendnp3::CommandStatus Supports(const opendnp3::AnalogOutputInt16& command, uint16_t index);// override final;
-	opendnp3::CommandStatus Perform(const opendnp3::AnalogOutputInt16& command, uint16_t index);// override final;
+	opendnp3::CommandStatus Supports(const opendnp3::AnalogOutputInt16& command, uint16_t index) override final;
+	opendnp3::CommandStatus Perform(const opendnp3::AnalogOutputInt16& command, uint16_t index) override final;
 
 
-	opendnp3::CommandStatus Supports(const opendnp3::AnalogOutputInt32& command, uint16_t index);// override final;
-	opendnp3::CommandStatus Perform(const opendnp3::AnalogOutputInt32& command, uint16_t index);// override final;
+	opendnp3::CommandStatus Supports(const opendnp3::AnalogOutputInt32& command, uint16_t index) override final;
+	opendnp3::CommandStatus Perform(const opendnp3::AnalogOutputInt32& command, uint16_t index) override final;
 
 
-	opendnp3::CommandStatus Supports(const opendnp3::AnalogOutputFloat32& command, uint16_t index);// override final;
-	opendnp3::CommandStatus Perform(const opendnp3::AnalogOutputFloat32& command, uint16_t index);// override final;
+	opendnp3::CommandStatus Supports(const opendnp3::AnalogOutputFloat32& command, uint16_t index) override final;
+	opendnp3::CommandStatus Perform(const opendnp3::AnalogOutputFloat32& command, uint16_t index) override final;
 
 
-	opendnp3::CommandStatus Supports(const opendnp3::AnalogOutputDouble64& command, uint16_t index);// override final;
-	opendnp3::CommandStatus Perform(const opendnp3::AnalogOutputDouble64& command, uint16_t index);// override final;
+	opendnp3::CommandStatus Supports(const opendnp3::AnalogOutputDouble64& command, uint16_t index) override final;
+	opendnp3::CommandStatus Perform(const opendnp3::AnalogOutputDouble64& command, uint16_t index) override final;
 	
 };
 
